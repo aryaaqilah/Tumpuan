103d6934a3db53fc9b3a0d2e2e457afd4d99090b
aryaaqilah <111030604+aryaaqilah@users.noreply.github.com>
2024-02-21 10:38:48 +0700

    Update login_page.dart

--- tumpuan/lib/login_page.dart
+++ tumpuan/lib/login_page.dart
@@ -241,7 +241,38 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
   Future<void> location() async {
+    final hasPermission = await _handleLocationPermission();
+    if (!hasPermission) return;
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
     print('test');
